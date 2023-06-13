ec914b2112bff636f3924eb3d3e6ab4d6283f28c
gaetangaetan <mrpoulpy@gmail.com>
2023-06-13 11:21:07 +0200

    test git 3

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 
 int flashInterval;
 
+uint8_t dmxChannels[512];
+
 typedef struct struct_message {
     uint8_t status;
     uint8_t data;    
@@ -15,6 +17,23 @@ typedef struct struct_message {
 struct_message incomingMessage;
 struct_message outgoingMessage;
 
+typedef struct struct_dmx_message {
+    uint8_t dmx001;
+    uint8_t dmx002;
+    uint8_t dmx003;
+    uint8_t dmx004;
+        
+} struct_dmx_message;
+
+struct_dmx_message incomingDMXMessage;
+
+typedef struct struct_dmx_packet {
+    uint8_t blockNumber; // on divise les 512 adresses en 4 blocs de 128 adresses
+    uint8_t dmxvalues[128];   
+} struct_dmx_packet;
+
+struct_dmx_packet incomingDMXPacket;
+
 
 void OnDataSent(u8 *mac_addr, u8 status) {  
      
@@ -23,11 +42,26 @@ void OnDataSent(u8 *mac_addr, u8 status) {
 
 // Callback when data is received
 void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
-  memcpy(&incomingMessage, incomingData, sizeof(incomingMessage));  
-  digitalWrite(LED_BUILTIN,LOW);
-  delay(100);
-  digitalWrite(LED_BUILTIN,HIGH);
-  Serial.println("data received");
+  memcpy(&incomingDMXPacket, incomingData, sizeof(incomingDMXPacket));  
+  uint8_t packetNumber = incomingDMXPacket.blockNumber;
+  for(int i=0;i<128;i++)
+  {
+    dmxChannels[(packetNumber*128)+i]=incomingDMXPacket.dmxvalues[i];
+  }
+  //  Serial.print(incomingDMXMessage.dmx001);
+  //  Serial.print(" ");
+  //  Serial.print(incomingDMXMessage.dmx002);
+  //  Serial.print(" ");
+  //  Serial.print(incomingDMXMessage.dmx003);
+  //  Serial.print(" ");
+  //  Serial.print(incomingDMXMessage.dmx004);
+  //  Serial.println(" ");
+   //delay(50);
+
+  // digitalWrite(LED_BUILTIN,LOW);
+  // delay(100);
+  // digitalWrite(LED_BUILTIN,HIGH);
+  // Serial.println("data received");
   //delay(incomingMessage.data); 
 }
 
@@ -65,8 +99,22 @@ uint8_t mrDMXRECEIVERAddress[] = {0x3C, 0x61, 0x05, 0xD1, 0xCC, 0x57};
 }
 
 void loop() {
+    digitalWrite(LED_BUILTIN,LOW);
+  delay(5);
+  digitalWrite(LED_BUILTIN,HIGH);  
+  delay(4*(255-dmxChannels[0])); 
+
   // put your main code here, to run repeatedly:
   // Serial.println("mrLEDTUBE receiver");
+  //  Serial.print(dmxChannels[0]);
+  //  Serial.print(" ");
+  //  Serial.print(dmxChannels[1]);
+  //  Serial.print(" ");
+  //  Serial.print(dmxChannels[2]);
+  //  Serial.print(" ");
+  //  Serial.print(dmxChannels[3]);
+  //  Serial.println(" ");
+  //  delay(20);
   // delay(1000);
 }
 
